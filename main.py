9f5812c2500bbf329fb4d507fc76eb968f6afe5e
Zhikai Chen <60167189+Czzzk@users.noreply.github.com>
2020-07-11 01:51:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello I like to make AI Apps'
+    return 'I am testing the auto deploy'
 
 @app.route('/name/<value>')
 def name(value):
